8e5246d18d0bdbc27f04697892b332a8acb9050c
Emmanuel <emrz98@gmail.com>
2021-08-08 10:17:28 -0500

    permutation through recursion

---
+++ Topics/permutation.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+void permutation_recursion(string a, int left, int right){
+    if(left == right){
+        cout<<a<<endl;
+    }
+    else{
+        for(int i = left; i<=right; i++){
+            swap(a[left], a[i]);
+            permutation_recursion(a, left + 1, right);
+            swap(a[left], a[i]);
+
+        }
+    }
+
+}
+
+
+int main(){
+    string str = "ABCDEF";
+    permutation_recursion(str, 0, str.size()-1);
+
+    return 0;
+}
