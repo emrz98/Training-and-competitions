4e7b4b7268a49db10df1af9659b33226507800c5
Emmanuel <emrz98@gmail.com>
2021-08-14 18:38:34 -0500

    Easter

--- Problems B/EasterEggs.cpp
+++ Problems B/EasterEggs.cpp
@@ -2,27 +2,38 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
-string  generate_permutations(string s, string& listPermutaion){ 
-    cout<<s<<endl;
-    return s;
-
-}
+string permutation_s = "";
+// void permutation(string word, int i){
+//     if(n == i){
+//         cout<<permutation_s<<endl;
+//     }
+//     for(int j = 0;j<i;j++){
+//         permutation_s.push_back()
+//         permutation_s.
+//     }
+// }
 
 int main(){
     int n;
     string eggs = "ROYGBIV";
-    string listPermutations[] = {};
+    sort(eggs.begin(), eggs.end());
     cin>>n;
-    listPermutations = generate_permutations(eggs, listPermutations);
-    for(int i = 0;i<n;i+=4){
-        if(n-i<=4){
-
+    int n_group = n/4;
+    int rest_group = n%4;
+    string sub_eggs = eggs.substr(0,4);
+    string last_eggs = eggs.substr(4,rest_group);
+    int counter = 0;
+    string res = "";
+    do{
+        if(counter == n_group){
+            if(rest_group == 0) break;
+            res+= last_eggs;
+            break;
         }
-        else{
+        res+=sub_eggs;
+        counter++;
+    }while(next_permutation(sub_eggs.begin(), sub_eggs.end()));
 
-        }
-    }
-
+    cout<<res<<endl;
     return 0;
 }
