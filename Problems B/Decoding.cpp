90588f6c53e283e4a6ac91ea616c5a76119247ef
Emmanuel <emrz98@gmail.com>
2021-08-14 19:48:58 -0500

    decoding complete

--- Problems B/Decoding.cpp
+++ Problems B/Decoding.cpp
@@ -5,44 +5,32 @@
 using namespace std;
 int main(){
     int n;
-    int pos;
     string s;
-    string res;
     cin>>n;
     cin>>s;
-    
-    
-
-    for(int i = n - 1;i >= 0;--i){
-        int l = abs(i-n);
-        if(l%2 == 0){
-            if(l){
-                pos = (l/2) - 1;
-            }
-        }
-        else{
-            if(l>1){
-                pos = l/2;
+    string newS="";
+    int len = s.length();
+    for(int i = 0;i<n;++i){
+        if(len % 2){
+            if(i%2 == 0){
+                newS+=s[i];
             }
             else{
-                pos = 0;
+                newS = s[i] + newS;
             }
-            
-        }
-        cout<<res<<" "<<pos<< " "<<s<< " "<<endl;
-        cout<<"after"<<endl;
-        string newWord;
-        if(res.length()>0){
-            newWord = res.substr(0, pos) + s[i] + res.substr(pos + 1, l - pos + 1); 
+
         }
         else{
-            newWord = s[i];
+            if(i%2 == 0){
+                newS = s[i] + newS;
+            }
+            else{
+                newS+=s[i];
+            }
         }
         
-        cout<<(newWord)<<endl;
-        // string newWord = s.substr(0,pos) + s.substr(pos + 1, length - pos + 1);
-        res = newWord;
+        
     }
-    cout<<res<<endl;
+    cout<<newS<<endl;
     return 0;
 }
